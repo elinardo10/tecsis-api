387b82c291e54de75fdc88d9489012e069e7668f
Elinardo Silva <elinardosilva@gmail.com>
2022-12-09 11:21:51 -0300

    workig in invoices api

--- routes/invoice.php
+++ routes/invoice.php
@@ -4,9 +4,10 @@ use App\V1\Invoice\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::prefix('customer')->group(function () {
-        Route::get('invoice', [InvoiceController::class, 'store']);
-        Route::get('invoice', [InvoiceController::class, 'show']);
-        Route::delete('invoice', [InvoiceController::class, 'delete']);
+    Route::prefix('invoices')->group(function () {
+        Route::get('', [InvoiceController::class, 'index']);
+        Route::post('', [InvoiceController::class, 'store']);
+        Route::get('{invoice}', [InvoiceController::class, 'show']);
+        Route::delete('{invoice}', [InvoiceController::class, 'delete']);
     });
 });
